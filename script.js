75c1340151f1374ebb86c4ea0e21cf13c5a2e6dc
sarabindalmook <145615356+sarabindalmook@users.noreply.github.com>
2023-11-04 22:33:20 +0400

    Sara: Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
+function initializeAudioBehavior(rectElement, audioFile) {
+    const audio = new Audio(audioFile);
+    let isPlaying = false;
 
+    function stopAudio() {
+        if (isPlaying) {
+            audio.pause();
+            isPlaying = false;
+        }
+    }
+
+    rectElement.addEventListener('click', () => {
+        if (isPlaying) {
+            audio.pause();
+            isPlaying = false;
+        } else {
+            audio.play();
+            isPlaying = true;
+        }
+    });
+
+    rectElement.addEventListener('mouseout', () => {
+        stopAudio();
+    });
+
+    document.addEventListener('mouseup', () => {
+        stopAudio();
+    });
+}
+
+// Usage
 const rect = document.querySelector('#a1b');
-const audio = new Audio('break.mp3'); 
+initializeAudioBehavior(rect, 'break.mp3');
 
-rect.addEventListener('click', () => {
-  audio.play(); 
-});
+const rect2 = document.querySelector('#a2b');
+initializeAudioBehavior(rect2, 'ORIGINAL_A2Baudio.mp3');
+
+const rect3 = document.querySelector('#bluehall');
+initializeAudioBehavior(rect3, 'piano.mp3');
+
+const rect4 = document.querySelector('#d2');
+initializeAudioBehavior(rect4, 'd2.mp3');
